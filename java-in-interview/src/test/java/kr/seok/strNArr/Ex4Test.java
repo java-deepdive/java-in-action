9ff9986bea8cc48714413d14cd9cbd1b3cac7b18
seokrae <kslbsh@gmail.com>
2021-01-22 18:46:00 +0900

    TIL - 회문순열, 하나빼기 테스트코드 작성

---
+++ java-in-interview/src/test/java/kr/seok/strNArr/Ex4Test.java
@@ -0,0 +1,36 @@
+package kr.seok.strNArr;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Ex4Test {
+
+    Ex4 ex4;
+    @BeforeEach
+    public void setUp() {
+        ex4 = new Ex4();
+    }
+    @Test
+    @DisplayName("Palindrome Permutation 테스트")
+    void testCase1() {
+
+        String[] strings = {
+                "Rats live on no evil star",
+                "A man, a plan, a canal, panama",
+                "Lleve",
+                "Tacotac",
+                "asda"
+        };
+
+        Arrays.stream(strings)
+                .forEach(s -> {
+                    boolean permutationOfPalindrome3 = ex4.isPermutationOfPalindrome3(s);
+                    System.out.println(permutationOfPalindrome3);
+                });
+    }
+}
---
+++ java-in-interview/src/test/java/kr/seok/strNArr/Ex5Test.java
@@ -0,0 +1,47 @@
+package kr.seok.strNArr;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Ex5Test {
+    Ex5 ex5;
+    @BeforeEach
+    public void setUp() {
+        ex5 = new Ex5();
+
+    }
+
+    @Test
+    @DisplayName("하나 빼기 테스트")
+    void testCase1() {
+        String[][] tests = {
+                {"a", "b", "true"},
+                {"", "d", "true"},
+                {"d", "de", "true"},
+                {"pale", "pse", "false"},
+                {"acdsfdsfadsf", "acdsgdsfadsf", "true"},
+                {"acdsfdsfadsf", "acdsfdfadsf", "true"},
+                {"acdsfdsfadsf", "acdsfdsfads", "true"},
+                {"acdsfdsfadsf", "cdsfdsfadsf", "true"},
+                {"adfdsfadsf", "acdfdsfdsf", "false"},
+                {"adfdsfadsf", "bdfdsfadsg", "false"},
+                {"adfdsfadsf", "affdsfads", "false"},
+                {"pale", "pkle", "true"},
+                {"pkle", "pable", "false"}
+        };
+
+        Arrays.stream(tests).forEach(
+                strings -> {
+                    boolean expected = "true".equals(strings[2]);
+                    String first = strings[0];
+                    String second = strings[1];
+                    assertThat(ex5.oneEditAway2(first, second)).isEqualTo(expected);
+                }
+        );
+    }
+}
