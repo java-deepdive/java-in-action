e9f64db474b0a4bdd31caf0bb62241e677d4140a
seokrae <kslbsh@gmail.com>
2021-01-22 18:33:59 +0900

    TIL - 회문순열, 하나빼기

---
+++ java-in-interview/src/main/java/kr/seok/strNArr/Ex4.java
@@ -0,0 +1,143 @@
+package kr.seok.strNArr;
+
+/**
+ * 회문 순열
+ * 주어진 문자열이 회문(palindrome)의 순열인지 확인하는 함수 작성
+ *
+ * 회문이란?
+ * - 회문이란 앞으로 읽으나 뒤로 읽으나 같은 단어 혹은 구절을 의미
+ *
+ * 순열이란?
+ * - 순열이란 문자열을 재배치하는 것
+ *
+ * 회문의 순열이란?
+ * - 문자열을 배치하여 회문이 가능한 단어
+ *
+ * 두 가지 개념을 합쳐 놓은 것
+ */
+public class Ex4 {
+    /*
+        어느 방향으로 읽어도 같은 문자열이 되도록 만들수 있는지 확인하기
+
+        해당 개념이 적용되기 위한 사전 조건
+            - 모든 문자가 짝수개 존재하는 경우 절반은 왼쪽에 나머지 절반은 오른쪽에 놓는 방법
+            - 홀수개인 경우, 단 하나의 문자만 존재하며 그 값이 중앙에 있고 나머지 값들을 양쪽에 놓는 방법
+     */
+
+    boolean isPermutationOfPalindrome(String phrase) {
+        int[] table = buildCharFrequencyTable(phrase);
+        return checkMaxOneOdd(table);
+    }
+
+
+    /**
+     * @return 각 문자가 몇 번 등장했는지 카운트 값을 저장한 배열을 반환
+     */
+    private int[] buildCharFrequencyTable(String phrase) {
+        //
+        int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+        for(char c : phrase.toCharArray()) {
+            int x = getCharNumber(c);
+            if(x != -1) {
+                table[x]++;
+            }
+        }
+        return table;
+    }
+
+    /**
+     * 각 문자에 숫자를 대응
+     * @return a -> 0, b -> 1 ... 대소문자 구분이 없고, 문자가 아닌 경우 -1로 대응
+     */
+    private int getCharNumber(char c) {
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(c);
+        if(a <= val && val <= z) {
+            return val - a;
+        }
+        return -1;
+    }
+
+    /**
+     * @return 홀수 문자가 한 개 이상 존재하는지 확인
+     */
+    private boolean checkMaxOneOdd(int[] table) {
+        boolean foundOdd = false;
+        // 문자 개수를 저장한 배열의 값을 반복하면서 odd를 찾으면 false를 반환
+        for(int count : table) {
+            if(count % 2 == 1) {
+                if(foundOdd) return false;
+                foundOdd = true;
+            }
+        }
+        return true;
+    }
+
+    /*
+        위 알고리즘의 탐색 시간은 O(n) 이다.
+
+        위 해법에서 문자열을 훑어나가면서 동시에 개수를 확인하도록 개선
+     */
+
+    boolean isPermutationOfPalindrome2(String phrase) {
+        int countOdd = 0;
+        int[] table = new int[
+                Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+
+        for(char c : phrase.toCharArray()) {
+            int x = getCharNumber(c);
+            if(x != -1) { // 해당 문자가 일단 범위 내에 존재.
+                table[x]++; // table에 존재하는 문자에 대한 count를 증가
+                if(table[x] % 2 == 1) { // table의 count가 홀수 인경우 count값을 증가
+                    countOdd++;
+                } else {
+                    countOdd--;
+                }
+            }
+        }
+        return countOdd <= 1;
+    }
+
+    /*
+        위 해법의 시간 복잡도는 변화가 없지만 해시테이블을 이용하였지만 각 문자마다 수행해야 할 코드가 늘어남
+
+
+        한 개의 정수 변수를 사용하여 비트 백터를 활용
+        알파벳을 0 ~ 25까지의 숫자로 치환하여 해당 문자가 등장할 때마다 치환된 위치의 비트값을 변경
+        마지막으로 한 개 이하의 비트가 1로 세팅되어 있는지 확인
+
+        이건 일단은 패스
+     */
+
+    boolean isPermutationOfPalindrome3(String phrase) {
+        int bitVector = createBitVector(phrase);
+        return bitVector == 0 || checkExactlyOneBitSet(bitVector);
+    }
+
+    private int createBitVector(String phrase) {
+        int bitVector = 0;
+        for(char c : phrase.toCharArray()) {
+            int x = getCharNumber(c);
+            bitVector = toggle(bitVector, x);
+        }
+        return bitVector;
+    }
+
+    private int toggle(int bitVector, int x) {
+        if(x < 0) return bitVector;
+        // 쉬프트 이동
+        int mask = 1 << x;
+        // and 연산 처리
+        if((bitVector & mask) == 0) {
+            bitVector |= mask;
+        } else {
+            bitVector &= ~mask;
+        }
+        return bitVector;
+    }
+
+    private boolean checkExactlyOneBitSet(int bitVector) {
+        return (bitVector & (bitVector - 1)) == 0;
+    }
+}
---
+++ java-in-interview/src/main/java/kr/seok/strNArr/Ex5.java
@@ -0,0 +1,112 @@
+package kr.seok.strNArr;
+
+/**
+ * 하나 빼기
+ * 문자열을 편집하는 방법
+ *  1. 문자 삽입
+ *  2. 문자 삭제
+ *  3. 문자 교체
+ *
+ *  문자열이 두 개 주어졌을 떄, 문자열을 같게 만들기 위해 편집 횟수가 1회 이내인지 확인하는 알고리즘
+ *
+ */
+public class Ex5 {
+
+    /*
+        brute force
+            - 한 번의 편집으로 생성할 수 있는 모든 문자열을 나열
+            - 한 문자열을 삭제, 교체, 삽입하면서 다른 문자열과 비교
+
+        각 연산이 무엇을 의미 하는지 생각해보기
+            - 교체: 두 문자열에서 단 하나의 문자만 다르다.
+            - 삽입: 특정 위치를 빈 공간으로 남겨두면 그 부분을 제외한 나머지 부분이 동일
+            - 삭제: 삭제의 반대는 삽입
+
+        접근 방법
+            - 문자열의 길이를 알고 있으면 어떤 연산이 필요한지 알 수 있다.
+
+     */
+
+   boolean oneEditAway(String first, String second) {
+       // 문자열 길이를 통해 필요한 해법을 선택
+       if(first.length() == second.length()) { // 교체
+           return oneEditReplace(first, second);
+
+       } else if(first.length() + 1 == second.length()) { // 삽입
+            return oneEditInsert(first, second);
+
+       } else if(first.length() - 1 == second.length()) { // 삭제
+           return oneEditInsert(second, first);
+       }
+       return false;
+   }
+
+    // 교체
+    private boolean oneEditReplace(String first, String second) {
+        boolean foundDifference = false;
+        for (int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) != second.charAt(i)) {
+                if (foundDifference) return false;
+                foundDifference = true;
+            }
+        }
+        return false;
+    }
+
+    // 삽입 또는 삭제
+    private boolean oneEditInsert(String first, String second) {
+        int idx1 = 0;
+        int idx2 = 0;
+        while (idx2 < second.length() && idx1 < first.length()) {
+            if (first.charAt(idx1) != second.charAt(idx2)) {
+                if (idx1 != idx2) {
+                    return false;
+                }
+                idx2++;
+            } else {
+                idx1++;
+                idx2++;
+            }
+        }
+        return true;
+    }
+
+    /*
+        중복되는 코드를 제거
+
+     */
+
+    boolean oneEditAway2(String first, String second) {
+        if(Math.abs(first.length() - second.length()) > 1) { // 편집을 위한 삽입, 삭제가 2번 이상이면 false 처리
+            return false;
+        }
+
+        String s1 = first.length() < second.length() ? first : second;
+        String s2 = first.length() < second.length() ? second : first;
+
+        int index1 = 0;
+        int index2 = 0;
+
+        boolean foundDifference = false;
+        // 문자열의 문자을 순서대로 체크
+        while (index2 < s2.length() && index1 < s1.length()) {
+
+            // 문자 비교. 같지 않은 경우
+            if(s1.charAt(index1) != s2.charAt(index2)) {
+
+                if(foundDifference) return false;
+                foundDifference = true;
+
+                // 교체의 경우 짧은 문자열의 포인터를 증가.
+                if(s1.length() == s2.length()) index1++;
+            } else {
+                // 문자가 같은 경우 짧은 문자열의 포인터 증가
+                index1++;
+            }
+            // 긴 문자열의 포인터는 항상 증가
+            index2++;
+        }
+        return true;
+    }
+
+}
