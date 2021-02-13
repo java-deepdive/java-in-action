1b50812b0ae87ca674148feb964744ec6b17d1cf
seokrae <kslbsh@gmail.com>
2021-02-13 19:41:24 +0900

    [interview] feat - simple queue

---
+++ java-in-interview/src/main/java/kr/seok/stackNQueue/SimpleQueue.java
@@ -0,0 +1,53 @@
+package kr.seok.stackNQueue;
+
+import java.util.NoSuchElementException;
+
+/**
+ * 단순 Queue
+ */
+public class SimpleQueue<T> {
+
+    private static class QueueNode<T> {
+        private T data;
+        private QueueNode<T> next;
+        public QueueNode(T data) {
+            this.data = data;
+        }
+    }
+
+    private QueueNode<T> first;
+    private QueueNode<T> last;
+
+    public void add(T item) {
+        QueueNode<T> t = new QueueNode<>(item);
+        if(last != null) {
+            last.next = t;
+        }
+        last = t;
+        if(first == null) {
+            first = last;
+        }
+    }
+
+    public T remove() {
+        if(first == null) throw new NoSuchElementException();
+
+        T data = first.data;
+        first = first.next;
+
+        if(first == null) {
+            last = null;
+        }
+
+        return data;
+    }
+
+    public T peek() {
+        if(first == null) throw new NoSuchElementException();
+        return first.data;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+}
