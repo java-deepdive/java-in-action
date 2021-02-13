b19d29df64c6f08b933b30871589793ce04eaf09
seokrae <kslbsh@gmail.com>
2021-02-13 19:40:15 +0900

    [leetCode] feat - 394 감 안옴

---
+++ java-in-leetCode/src/main/java/kr/seok/stack/Stack_394.java
@@ -0,0 +1,139 @@
+package kr.seok.stack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Given an encoded string, return its decoded string.
+ *
+ * The encoding rule is:
+ *  k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times.
+ * Note that k is guaranteed to be a positive integer.
+ *
+ * You may assume that the input string is always valid;
+ * No extra white spaces, square brackets are well-formed, etc.
+ *
+ * Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers,
+ * k. For example, there won't be input like 3a or 2[4].
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "3[a]2[bc]"
+ * Output: "aaabcbc"
+ * Example 2:
+ *
+ * Input: s = "3[a2[c]]"
+ * Output: "accaccacc"
+ * Example 3:
+ *
+ * Input: s = "2[abc]3[cd]ef"
+ * Output: "abcabccdcdcdef"
+ * Example 4:
+ *
+ * Input: s = "abc3[cd]xyz"
+ * Output: "abccdcdcdxyz"
+ *
+ */
+public class Stack_394 {
+    public String decodeString_oneStack(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ']') {
+                List<Character> decodedString = new ArrayList<>();
+                // get the encoded string
+                while (stack.peek() != '[') {
+                    decodedString.add(stack.pop());
+                }
+                // pop [ from the stack
+                stack.pop();
+                int base = 1;
+                int k = 0;
+                // get the number k
+                while (!stack.isEmpty() && Character.isDigit(stack.peek())) {
+                    Character pop = stack.pop();
+                    System.out.println(("(pop - '0') :: " + (pop - '0')));
+                    System.out.println("(pop - '0') * base :: " + (pop - '0') * base);
+                    k = k + (pop - '0') * base;
+                    System.out.println("k :: " + k);
+                    base *= 10;
+                }
+                // decode k[decodedString], by pushing decodedString k times into stack
+                while (k != 0) {
+                    for (int j = decodedString.size() - 1; j >= 0; j--) {
+                        stack.push(decodedString.get(j));
+                    }
+                    k--;
+                }
+            }
+            // push the current character to stack
+            else {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        // get the result from stack
+        char[] result = new char[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        return new String(result);
+    }
+
+    String decodeString_twoStack(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+
+        int k = 0;
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + ch - '0';
+            } else if (ch == '[') {
+                // push the number k to countStack
+                countStack.push(k);
+                // push the currentString to stringStack
+                stringStack.push(currentString);
+                // reset currentString and k
+                currentString = new StringBuilder();
+                k = 0;
+            } else if (ch == ']') {
+                StringBuilder decodedString = stringStack.pop();
+                // decode currentK[currentString] by appending currentString k times
+                for (int currentK = countStack.pop(); currentK > 0; currentK--) {
+                    decodedString.append(currentString);
+                }
+                currentString = decodedString;
+            } else {
+                currentString.append(ch);
+            }
+        }
+        return currentString.toString();
+    }
+
+    int index = 0;
+    String decodeString_recursive(String s) {
+        StringBuilder result = new StringBuilder();
+        while (index < s.length() && s.charAt(index) != ']') {
+            if (!Character.isDigit(s.charAt(index)))
+                result.append(s.charAt(index++));
+            else {
+                int k = 0;
+                // build k while next character is a digit
+                while (index < s.length() && Character.isDigit(s.charAt(index)))
+                    k = k * 10 + s.charAt(index++) - '0';
+                // ignore the opening bracket '['
+                index++;
+                String decodedString = decodeString_recursive(s);
+                // ignore the closing bracket ']'
+                index++;
+                // build k[decodedString] and append to the result
+                while (k-- > 0)
+                    result.append(decodedString);
+            }
+        }
+        return new String(result);
+    }
+}
