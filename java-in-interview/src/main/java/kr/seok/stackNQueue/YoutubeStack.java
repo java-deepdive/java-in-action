38a0d6a30e41b83c916ddf4afbe92c8b3cd91406
seokrae <kslbsh@gmail.com>
2021-02-17 18:11:39 +0900

    [interview] refactor - stack 알고리즘 추가

--- java-in-interview/src/main/java/kr/seok/stackNQueue/YoutubeStack.java
+++ java-in-interview/src/main/java/kr/seok/stackNQueue/YoutubeStack.java
@@ -1,39 +1,236 @@
 package kr.seok.stackNQueue;
 
+import java.util.ArrayList;
 import java.util.EmptyStackException;
+import java.util.Stack;
 
-public class YoutubeStack<T> {
+public class YoutubeStack {
 
-    class Node<T> {
-        private T data;
-        private Node<T> next;
+    // 스택 정렬, asc
+    static class SortStack {
+        static void sort(Stack<Integer> s1) {
+            Stack<Integer> s2 = new Stack<>();
+            while (!s1.isEmpty()) { // 비어있지 않은 경우 반복
+                int tmp = s1.pop();
 
-        public Node(T data) {
-            this.data = data;
+                while (!s2.isEmpty() && s2.peek() > tmp) { // s2의 top에 있는 값이 tmp 보다 큰 경우 s1 스택에 넣는다.
+                    s1.push(s2.pop());
+                }
+                s2.push(tmp);
+            }
+            while (!s2.isEmpty()) {
+                s1.push(s2.pop());
+            }
         }
     }
 
-    private Node<T> top;
+    /*
+        분석 해야 함
+     */
+    static class SetOfStacks {
+        int capacity;
+        ArrayList<Stack<Integer>> stacks = new ArrayList<>();
 
-    public T pop() {
-        if(top == null) throw new EmptyStackException();
-        T item = top.data;
-        top = top.next;
-        return item;
+        SetOfStacks(int capacity) {
+            this.capacity = capacity;
+        }
+
+        // 마지막 스택을 조회하기 위한 메서드
+        public Stack<Integer> getLastStack() {
+            if(stacks.size() == 0) return null;
+            return stacks.get(stacks.size() - 1);
+        }
+
+        // 스택을 추가한다는 말은 ArrayList에 새로운 스택을 추가한다는 말과 같다.
+        public void addStack() {
+            stacks.add(new Stack<>());
+        }
+
+        // ArrayList의 마지막에 존재하는 스택을 삭제
+        public void removeLastStack() {
+            stacks.remove(stacks.size() - 1);
+        }
+
+        //
+        public void push(int data) {
+            Stack<Integer> last = getLastStack();
+            if(last == null || last.size() == capacity) {
+                addStack();
+                last = getLastStack();
+            }
+            last.push(data);
+        }
+
+        public int pop() {
+
+            Stack<Integer> last = getLastStack();
+            if(last == null || last.isEmpty()) throw new EmptyStackException();
+
+            int data = last.pop();
+
+            // pop하고 난 뒤 stack이 비어 있으면 arrayList 삭제
+            if(last.isEmpty()) removeLastStack();
+
+            return data;
+        }
     }
 
-    public void push(T item) {
-        Node<T> t = new Node<T>(item);
-        t.next = top;
-        top = t;
+    /*
+        Stack을 이용한 최소 값 찾기 알고리즘
+
+        메인 Stack과 입력마다 값을 비교하여 최소 값을 저장하는 스택을 생성하여 관리하는 방식
+     */
+    static class MinTwoStack extends Stack<Integer> {
+
+        Stack<Integer> stack;
+        public MinTwoStack() {
+            this.stack = new Stack<>();
+        }
+
+        public int min() {
+            if(stack.isEmpty()) return Integer.MAX_VALUE;
+            else return stack.peek();
+        }
+
+        public void push(int value) {
+            if (value < min()) {
+                stack.push(value);
+            }
+            super.push(value);
+        }
+
+        public Integer pop() {
+            int value = super.pop();
+            if(value == min()) {
+                stack.pop();
+            }
+            return value;
+        }
     }
 
-    public T peek() {
-        if(top == null) throw new EmptyStackException();
-        return top.data;
+    static class MinStack extends Stack<MinStack.MinNode> {
+        static class MinNode {
+            int value;
+            int min;
+
+            public MinNode(int value, int min) {
+                this.value = value;
+                this.min = min;
+            }
+        }
+
+        public int min() {
+            if(this.isEmpty()) {
+                return Integer.MAX_VALUE;
+            } else {
+                return peek().min;
+            }
+        }
+
+        public void push(int value) {
+            int newMin = Math.min(value, min());
+            System.out.println("현재 값: " + value + " :: 최소값: " + newMin);
+            super.push(new MinNode(value, newMin));
+        }
     }
 
-    public boolean isEmpty () {
-        return top == null;
+    static class LinkedListBasedStack<T> {
+
+        static class Node<T> {
+            private T data;
+            private Node<T> next;
+
+            public Node(T data) {
+                this.data = data;
+            }
+        }
+
+        private Node<T> top;
+
+        public T pop() {
+            if(top == null) throw new EmptyStackException();
+            T item = top.data;
+            top = top.next;
+            return item;
+        }
+
+        public void push(T item) {
+            Node<T> t = new Node<T>(item);
+            t.next = top;
+            top = t;
+        }
+
+        public T peek() {
+            if(top == null) throw new EmptyStackException();
+            return top.data;
+        }
+
+        public boolean isEmpty () {
+            return top == null;
+        }
     }
+
+    // 고정 길이 배열 스택
+    static class ArrayBasedStack {
+
+        static class FullStackException extends RuntimeException {
+            public FullStackException() { }
+
+            public FullStackException(String message) {
+                super(message);
+            }
+        }
+        private int numOfStacks = 3; // 스택의 개수는 3개로 고정
+        private int stackSize; // 각 스택에 사이즈 저장
+        private int[] values; // 실제 데이터 필드
+        private int[] sizes; // 각 스택의 데이터 사이즈를 저장할 공간
+
+        public ArrayBasedStack(int stackSize) {
+            this.stackSize = stackSize;
+            this.sizes = new int[numOfStacks]; // 각 스택에 데이터 사이즈를 저장
+            this.values = new int[numOfStacks * stackSize]; // 실제 데이터를 저장할 공간을 생성
+        }
+
+        public boolean isEmpty(int stackNum) {
+            return sizes[stackNum] == 0;
+        }
+
+        public boolean isFull(int stackNum) {
+            return sizes[stackNum] == stackSize;
+        }
+
+        public int getTopIndex(int stackNum) {
+            int offset = stackSize * stackNum;
+            int size = sizes[stackNum];
+
+            return offset + size - 1;
+        }
+
+        public void push(int stackNum, int data) {
+            if(isFull(stackNum)) throw new FullStackException();
+
+            // top 인덱스를 가져와 하나 증가시킨 후, 그 자리에 data를 대입
+            values[getTopIndex(stackNum) + 1] = data;
+            sizes [stackNum]++;
+        }
+
+        public int pop(int stackNum) {
+            if(isEmpty(stackNum)) throw new EmptyStackException();
+
+            int top = getTopIndex(stackNum);
+            int data = values[top];
+            values[top] = 0;
+            sizes[stackNum]--;
+
+            return data;
+        }
+
+        public int peek(int stackNum) {
+            if(isEmpty(stackNum)) throw new EmptyStackException();
+            return values[getTopIndex(stackNum)];
+        }
+    }
+
 }
+
+
---
+++ java-in-interview/src/test/java/kr/seok/stackNQueue/YoutubeStackTest.java
@@ -0,0 +1,71 @@
+package kr.seok.stackNQueue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+
+class YoutubeStackTest {
+
+    @Test
+    @DisplayName("스택 메서드 확인 테스트")
+    void testCase1() {
+        YoutubeStack.ArrayBasedStack arrStack = new YoutubeStack.ArrayBasedStack(5);
+
+        try {
+            arrStack.push(0, 1);
+            arrStack.push(0, 2);
+            arrStack.push(0, 3);
+            arrStack.push(0, 4);
+            arrStack.push(0, 5);
+
+            arrStack.push(1, 11);
+            arrStack.push(1, 12);
+            arrStack.push(1, 13);
+            arrStack.push(1, 14);
+            arrStack.push(1, 15);
+
+        } catch(YoutubeStack.ArrayBasedStack.FullStackException e) {
+            System.out.println("Exception :: " + e);
+        }
+
+        try {
+            System.out.println("stack : #0 " + arrStack.pop(0));
+            System.out.println("stack : #0 " + arrStack.pop(0));
+            System.out.println("stack : #0 " + arrStack.peek(0));
+            System.out.println("stack : #0 " + arrStack.pop(0));
+            System.out.println("stack : #0 " + arrStack.isEmpty(0));
+            System.out.println("stack : #0 " + arrStack.pop(0));
+            System.out.println("stack : #0 " + arrStack.pop(0));
+            System.out.println("stack : #0 " + arrStack.isEmpty(0));
+
+            System.out.println("stack : #1 " + arrStack.pop(1));
+            System.out.println("stack : #1 " + arrStack.pop(1));
+            System.out.println("stack : #1 " + arrStack.peek(1));
+            System.out.println("stack : #1 " + arrStack.pop(1));
+            System.out.println("stack : #1 " + arrStack.isEmpty(1));
+            System.out.println("stack : #1 " + arrStack.pop(1));
+            System.out.println("stack : #1 " + arrStack.pop(1));
+            System.out.println("stack : #1 " + arrStack.isEmpty(1));
+
+        } catch(EmptyStackException e) {
+            System.out.println("isEmpty");
+        }
+    }
+
+    @Test
+    @DisplayName("최소 값 반환하기")
+    void testCase2() {
+        YoutubeStack.MinStack minStack = new YoutubeStack.MinStack();
+
+        minStack.push(3);
+        minStack.push(5);
+        minStack.push(1);
+        minStack.push(2);
+
+        System.out.println("min : " + minStack.min());
+        System.out.println(minStack.pop().value);
+        System.out.println(minStack.pop().value);
+        System.out.println("min : " + minStack.min());
+    }
+}
