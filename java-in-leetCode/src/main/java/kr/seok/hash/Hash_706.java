4fd8f88edf2f272e12baab9b0fba733efdc9f1d3
seokrae <kslbsh@gmail.com>
2021-02-28 01:30:07 +0900

    [interview] feat - hashMap

--- java-in-leetCode/src/main/java/kr/seok/hash/Hash_706.java
+++ java-in-leetCode/src/main/java/kr/seok/hash/Hash_706.java
@@ -5,16 +5,15 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Hash_706 {
-    static class Pair<U, V> {
-        public U first;
+    static class Pair<K, V> {
+        public K first;
         public V second;
 
-        public Pair(U first, V second) {
+        public Pair(K first, V second) {
             this.first = first;
             this.second = second;
         }
     }
-
 
     static class Bucket {
         private List<Pair<Integer, Integer>> bucket;
@@ -57,7 +56,6 @@ public class Hash_706 {
         private int key_space;
         private List<Bucket> hash_table;
 
-        /** Initialize your data structure here. */
         public MyHashMap() {
             this.key_space = 2069;
             this.hash_table = new ArrayList<Bucket>();
@@ -66,32 +64,25 @@ public class Hash_706 {
             }
         }
 
-        /** value will always be non-negative. */
         public void put(int key, int value) {
-            int hash_key = key % this.key_space;
+            int hash_key = getHashCode(key);
             this.hash_table.get(hash_key).update(key, value);
         }
 
-        /**
-         * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping
-         * for the key
-         */
         public int get(int key) {
-            int hash_key = key % this.key_space;
+            int hash_key = getHashCode(key);
             return this.hash_table.get(hash_key).get(key);
         }
 
-        /** Removes the mapping of the specified value key if this map contains a mapping for the key */
         public void remove(int key) {
-            int hash_key = key % this.key_space;
+            int hash_key = getHashCode(key);
             this.hash_table.get(hash_key).remove(key);
         }
-    }
 
-    /**
-     * Your MyHashMap object will be instantiated and called as such: MyHashMap obj = new MyHashMap();
-     * obj.put(key,value); int param_2 = obj.get(key); obj.remove(key);
-     */
+        private int getHashCode(int key) {
+            return key % this.key_space;
+        }
+    }
 
     public static void main(String[] args) {
         MyHashMap hashMap = new MyHashMap();
