3bc1da12be3dee7e5d1f5a2f4c0b843f15398d86
seokrae <kslbsh@gmail.com>
2021-03-20 22:53:29 +0900

    [tdd] lotto 다시

--- java-in-tdd/src/main/java/kr/seok/lotto/exception/LottoException.java
+++
@@ -1,8 +0,0 @@
-package kr.seok.lotto.exception;
-
-public class LottoException extends RuntimeException {
-    public LottoException() { }
-    public LottoException(String message) {
-        super(message);
-    }
-}
