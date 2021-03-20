1eabe4f010ccd074931c20c8dbb3634c5236b6cc
seokrae <kslbsh@gmail.com>
2021-03-20 22:26:16 +0900

    [oop] refactor: bag으로 책임 위임

--- java-in-oop/src/main/java/kr/seok/ticket/Bag.java
+++ java-in-oop/src/main/java/kr/seok/ticket/Bag.java
@@ -14,7 +14,18 @@ public class Bag {
         this.invitation = invitation;
     }
 
-    public boolean hasInvitation() {
+    public Long hold(Ticket ticket) {
+        if(hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
+
+    private boolean hasInvitation() {
         return invitation != null;
     }
 
@@ -22,11 +33,11 @@ public class Bag {
         return ticket != null;
     }
 
-    public void setTicket(Ticket ticket) {
+    private void setTicket(Ticket ticket) {
         this.ticket = ticket;
     }
 
-    public void minusAmount(Long amount) {
+    private void minusAmount(Long amount) {
         this.amount -= amount;
     }
 
