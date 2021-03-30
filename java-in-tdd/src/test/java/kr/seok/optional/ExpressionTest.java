cd39457aefb7d144b97cc141c8c8d5ec92f07b08
seokrae <kslbsh@gmail.com>
2021-03-30 21:38:16 +0900

    [tdd] 1단계

---
+++ java-in-tdd/src/test/java/kr/seok/optional/ExpressionTest.java
@@ -0,0 +1,26 @@
+package kr.seok.optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+
+public class ExpressionTest {
+
+    @DisplayName(value = "Expression의 정의된 연산을 조회 테스트")
+    @Test
+    public void of() {
+        assertThat(Expression.PLUS == Expression.of("+")).isTrue();
+    }
+
+    @DisplayName(value = "Expression의 정의된 연산이 아닌 경우 예외 테스트")
+    @Test
+    public void notValidExpression() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> {
+                    Expression.of("&");
+                });
+    }
+}
---
+++ java-in-tdd/src/test/java/kr/seok/optional/UsersTest.java
@@ -0,0 +1,23 @@
+package kr.seok.optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UsersTest {
+
+    @DisplayName(value = "getUser() filtering 테스트")
+    @Test
+    public void getUser() {
+        Users users = new Users();
+        assertThat(users.getUser("crong")).isEqualTo(new User("crong", 35));
+    }
+
+    @DisplayName(value = "getUser() filtering default 값 확인 테스트")
+    @Test
+    public void getDefaultUser() {
+        Users users = new Users();
+        assertThat(users.getUser("codesquard")).isEqualTo(Users.DEFAULT_USER);
+    }
+}
