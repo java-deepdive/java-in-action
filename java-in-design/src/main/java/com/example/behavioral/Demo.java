5aa1eedec0f9ffc4acc2d910b34e60a7ed882685
seokrae <kslbsh@gmail.com>
2021-05-31 17:13:22 +0900

    feat: chain of responsibility pattern

---
+++ java-in-design/src/main/java/com/example/behavioral/Demo.java
@@ -0,0 +1,47 @@
+package com.example.behavioral;
+
+import com.example.behavioral.middleware.Middleware;
+import com.example.behavioral.middleware.RoleCheckMiddleware;
+import com.example.behavioral.middleware.ThrottlingMiddleware;
+import com.example.behavioral.middleware.UserExistsMiddleware;
+import com.example.behavioral.server.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Demo class. Everything comes together here.
+ */
+public class Demo {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static Server server;
+
+    private static void init() {
+        server = new Server();
+        server.register("admin@example.com", "admin_pass");
+        server.register("user@example.com", "user_pass");
+
+        // All checks are linked. Client can build various chains using the same
+        // components.
+        Middleware middleware = new ThrottlingMiddleware(2);
+        middleware.linkWith(new UserExistsMiddleware(server))
+                .linkWith(new RoleCheckMiddleware());
+
+        // Server gets a chain from client code.
+        server.setMiddleware(middleware);
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        boolean success;
+        do {
+            System.out.print("Enter email: ");
+            String email = reader.readLine();
+            System.out.print("Input password: ");
+            String password = reader.readLine();
+            success = server.logIn(email, password);
+        } while (!success);
+    }
+}
