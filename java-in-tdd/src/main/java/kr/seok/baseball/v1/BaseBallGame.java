b1ae0fe8cfca6a81b56d3edb04816fcdbd4b013d
seokrae <kslbsh@gmail.com>
2022-08-08 12:20:40 +0900

    feat: 숫자야구 절차지향 메서드 수준의 기능 분리

--- java-in-tdd/src/main/java/kr/seok/baseball/v1/BaseBallGame.java
+++ java-in-tdd/src/main/java/kr/seok/baseball/v1/BaseBallGame.java
@@ -1,5 +1,6 @@
 package kr.seok.baseball.v1;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -9,39 +10,52 @@ public class BaseBallGame {
 	
 	private final Scanner in = new Scanner(System.in);
 	
-	public void run() {
-		System.out.println("게임 시작");
-		
-		// 랜덤한 숫자 3개 생성
-		System.out.println("컴퓨터 숫자 생성");
-		List<Integer> com = com();
-		
-		while(true) {
-			
-			System.out.println("사용자 숫자 입력");
-			String[] user = validUserInput();
-			
-			printReport(com, user);
-			
-			if(isReGame()) {
-				break;
-			}
-		}
-		
+	public static void main(String[] args) {
+		new BaseBallGame().run();
 	}
 	
-	private void printReport(List<Integer> com, String[] user) {
-		if(isMatches(com, user) == 3) {
-			System.out.println("3 스트라이크 -> 게임 종료");
-		} else if(isMatches(com, user) != 3) {
-			System.out.println("user = " + user);
+	public void run() {
+		do {
+			System.out.println("게임 시작");
+			System.out.println("컴퓨터 숫자 생성");
+			
+			startGame(com()); // 게임 시작 (랜덤한 숫자 3개 생성하여 게임 시작)
+			
+			
+		} while (!isReGame());
+	}
+	
+	private void startGame(List<Integer> com) {
+		System.out.println("com = " + com);
+		String[] user;
+		do {
+			System.out.println("사용자 숫자 입력");
+			user = validUserInput(); // 사용자 입력
+			
+		} while (round(com, user) != 3); // 사용자 & 컴퓨터 숫자 매핑하여 strike 3인지 확인
+	}
+	
+	private int round(List<Integer> com, String[] user) { // 컴퓨터 & 사람 숫자 매핑
+		int strike = 0;
+		int ball = 0;
+		for (int i = 0; i < user.length; i++) {
+			int useNum = Integer.parseInt(user[i]);
+			if (useNum == com.get(i)) {
+				strike++;
+			} else {
+				if (com.contains(useNum)) {
+					ball++;
+				}
+			}
 		}
+		System.out.println(strike + " 스트라이크 : " + ball + " 볼");
+		return strike;
 	}
 	
 	private String[] validUserInput() {
 		String[] user = in.nextLine().split("");// 입력 방식 123
 		
-		if(user.length != 3) {
+		if (user.length != 3) {
 			throw new RuntimeException("수가 부족");
 		}
 		
@@ -55,20 +69,16 @@ public class BaseBallGame {
 	}
 	
 	private boolean isReGame() {
+		System.out.println("재시작 여부 ex) exit");
 		String nextLine = in.nextLine();
 		return "exit".equals(nextLine);
 	}
 	
-	private int isMatches(List<Integer> com, String[] user) {
-		
-		
-		return 0;
-	}
-	
 	public List<Integer> com() {
-		return IntStream.rangeClosed(1, 9)
+		List<Integer> collect = IntStream.rangeClosed(1, 9)
 			.boxed()
-			.limit(3)
 			.collect(Collectors.toList());
+		Collections.shuffle(collect);
+		return collect.subList(0, 3);
 	}
 }
